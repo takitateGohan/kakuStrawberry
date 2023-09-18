4cfeddaa5dc49d4e5f3b2f30197fec5dc578ebb2
omine <omine@example.com>
2023-09-18 16:44:45 +0900

    不要なファイルの削除

--- js/script.js
+++
@@ -1,5 +0,0 @@
-$(function(){
-    $.ajaxSetup({cache:false});
-    $("header").load("https://takitateGohan.github.io/kakuStrawberry/common/header.html");
-    $("footer").load("https://takitateGohan.github.io/kakuStrawberry/common/footer.html");
-});
